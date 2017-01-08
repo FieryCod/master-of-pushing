b7f82ce75f8fec03bf7832ac1b6d8ccf5af1a4ae
tkulpa <tomasz.kulpa.praca@gmail.com>
2017-01-08 23:17:29 +0100

    Add basic rounds implementation

--- src/scripts/classes/Player.ts
+++ src/scripts/classes/Player.ts
@@ -1,5 +1,6 @@
 export class Player extends Phaser.Sprite {
     public startingPosition: Phaser.Point;
+    public lastTouchedBy: Player;
     constructor(game: Phaser.Game, x, y, key) {
         super(game, x, y, key);
         this.startingPosition = new Phaser.Point(x, y);
