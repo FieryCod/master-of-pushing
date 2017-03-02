385ba7a22385e0c298f36c7580ef44047477ee31
FieryCod <kwcharllie379@gmail.com>
2017-03-02 17:45:57 +0100

    Resolve an issue #20

--- src/scripts/classes/Player.ts
+++ src/scripts/classes/Player.ts
@@ -28,7 +28,7 @@ export class Player extends Phaser.Sprite {
         this.weaponName = CONFIG.WEAPON_BOMB;
         this.playerColor = playerColor;
     }
-    public postionAtStart() {
+    public positionAtStart() {
         this.body.setZeroVelocity();
         this.body.setZeroRotation();
         this.body.angle = 0;
@@ -41,7 +41,7 @@ export class Player extends Phaser.Sprite {
         this.body.dynamic = true;
         this.alpha = 1;
         this.scale.x = this.scale.y = ORIGINAL_SCALE;
-        this.postionAtStart();
+        this.positionAtStart();
         return <Player>super.revive(health);
 
     }
@@ -51,8 +51,7 @@ export class Player extends Phaser.Sprite {
             this.body.dynamic = false;
             this.locked = true;
             let tweenMove, tweenFade, tweenScale: Phaser.Tween;
-            let velocityX = this.body.velocity.x, velocityY = this.body.velocity.y;
-            let directionX = this.body.x + velocityX, directionY = this.body.y + velocityY;
+            let directionX = this.body.x + this.body.velocityX, directionY = this.body.y + this.body.velocityY;
             tweenMove = this.game.add.tween(this).to({ x: directionX, y: directionY }, 1000, Phaser.Easing.Linear.None, true);
             tweenScale = this.game.add.tween(this.scale).to({ x: 0.3, y: 0.3 }, 900, Phaser.Easing.Cubic.In, true, 100);
             tweenMove.onComplete.addOnce(() => {
@@ -60,6 +59,5 @@ export class Player extends Phaser.Sprite {
                     super.kill();
                 });
             });
-        
     }
 }
--- src/scripts/state/Main.ts
+++ src/scripts/state/Main.ts
@@ -24,11 +24,13 @@ export class Main extends Phaser.State {
     private space: Phaser.Key;
     private weaponManager: WeaponManager;
     private scoreboard: Scoreboard;
+    private onRoundEnd: Phaser.Signal;
 
     public init(rounds: number) {
         this.scoreboard = new Scoreboard(this.game);
         this.rounds = rounds || 2;
-        this.currentRound = 0;
+        this.currentRound = 1;
+        this.onRoundEnd = new Phaser.Signal();
         this.players = new Phaser.Group(this.game);
         this.cursors = this.game.input.keyboard.createCursorKeys();
         this.arena = new Arena(this.game, this.world.centerX, this.world.centerY, this.world.height - 100);
@@ -47,14 +49,17 @@ export class Main extends Phaser.State {
         this.addPlayer("WOJTAS", "#FFF000");
         this.addPlayer("KAROLIX", "#FF00F0");
         this.addPlayer("PAWEŁ", "#FF00FF");
-
-        this.controlledPlayer = <Player>this.players.getChildAt(0);
+        this.onRoundEnd.add(() => {
+            setTimeout(() => {
+                this.nextRound();
+            }, 2000);
+        }, this);
         this.initPhysics();
+
+        this.controlledPlayer = <Player>this.players.getChildAt(0); // tymczasowe
         this.assignStartPositionsToPlayers();
-        this.players.callAll("postionAtStart", null);
         this.roundStartTimer.start();
         this.roundStartTimer.startRoundCountdown();
-        ++this.currentRound;
         this.game.world.bringToTop(this.players);
         this.scoreboard.drawScoreboard();
     }
@@ -74,7 +79,7 @@ export class Main extends Phaser.State {
     }
     private addPlayer(name: string, playerColor: string) {
         let player = new Player(this.game, this.arena.x, this.arena.y, name, playerColor, Main.numberOfPlayers++);
-        player.events.onKilled.add(this.isRoundEnd, this, 1);
+        player.events.onKilled.add(this.roundEnd, this, 1);
         this.players.add(player);
         this.scoreboard.addPlayerToScoreboard(name, playerColor);
     }
@@ -89,6 +94,7 @@ export class Main extends Phaser.State {
             player.body.x = player.startPosition.x = this.arena.x + (this.arena.collision.radius - CONFIG.START_POS_EDGE_OFFSET) * Math.cos(angle * (Math.PI / 180));
             player.body.y = player.startPosition.y = this.arena.y + (this.arena.collision.radius - CONFIG.START_POS_EDGE_OFFSET) * Math.sin(angle * (Math.PI / 180));
         }
+        this.players.callAll("positionAtStart", null);
     };
     public update() {
         this.players.forEachAlive(player => {
@@ -118,35 +124,34 @@ export class Main extends Phaser.State {
         }
     }
 
-    private isRoundEnd(): void {
-       // FIXME: Bład #20 jest spowodowany tym, że nextRound jest wywoływane już gdy jest tylko jeden player wiec jezeli on szybko zginie to 
-       // i tak juz jest nowa runda. Aby to naprawić to albo trzeba sprawdzic bo np sekundzie czy dalej jest jeden zawodnik albo zapauzowac 
-       // lub w jakis sposob przewidziec czy spadnie
-        if (this.players.countLiving() <= 1) {
+    private roundEnd(): void {
+        if (this.players.countLiving() === 1) {
             let roundSurvivor: Player = this.players.getFirstAlive();
             // ++roundSurvivor.scores; // TODO:: Better to use a function which will increment internal player scores
-            this.scoreboard.updateInfo(roundSurvivor.playerIndex)
+            this.scoreboard.updateInfo(roundSurvivor.playerIndex);
 
             if (this.currentRound >= this.rounds) {
-                // for(let i in this.players.get)
                 // TODO: Finish game and redirect to score screen;
                 this.showGameResults();
             }
-            else{
-                this.nextRound();
-            } 
+            else {
+                this.killAll();
+                this.onRoundEnd.dispatch();
+            }
         }
     }
     private killAll() {
         this.players.callAll("kill", null);
     }
     private nextRound() {
-    
         ++this.currentRound;
         this.arena.reset();
         this.gameTimer.reset();
         this.roundStartTimer.startRoundCountdown();
         this.players.callAll("revive", null);
+
+    }
+    private showRoundWinner(winner) {
 
     }
     private playersCollideCallback(playerBody1: Phaser.Physics.P2.Body, playerBody2: Phaser.Physics.P2.Body) {
